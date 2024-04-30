168df2f9f92b41c2cb667fa4c95db4e12eef3237
wojtaz31 <wojtaz31@wp.pl>
2024-05-01 00:10:59 +0200

    add method that randomly selects one-mast ship and excludes adjacent tiles from further draws

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-enum TileState {
+enum TileState
+{
     const FREE = 0b00;
     const EXCLUDED = 0b01;
     const OCCUPIED = 0b10;
@@ -9,30 +10,30 @@ enum TileState {
 class Board
 {
     public int $width;
-    public int $heigth;
+    public int $height;
     public int $pxlTileSize;
     public array $tiles;
 
-    function __construct(int $width = 10, int $heigth = 10, int $pxlTileSize = 80)
+    function __construct(int $width = 10, int $height = 10, int $pxlTileSize = 80)
     {
         $this->width = $width;
-        $this->heigth = $heigth;
+        $this->height = $height;
         $this->pxlTileSize = $pxlTileSize;
-        for ($i = 0; $i < $width; $i++) $this->tiles[$i] = array_fill(0, $heigth, TileState::FREE);
+        for ($i = 0; $i < $width; $i++) $this->tiles[$i] = array_fill(0, $height, TileState::FREE);
     }
 
     function inject_board_styles()
-    {   
+    {
         echo '<link rel="stylesheet" href="styles.css">';
         echo "<style>";
         echo "#board-container {";
         echo "    display: grid;";
-        echo "    grid-template-columns: repeat($this->width, $this->pxlTileSize"."px);";
-        echo "    grid-template-rows: repeat($this->heigth, $this->pxlTileSize"."px);";
+        echo "    grid-template-columns: repeat($this->width, $this->pxlTileSize" . "px);";
+        echo "    grid-template-rows: repeat($this->height, $this->pxlTileSize" . "px);";
         echo "    justify-content: center;";
         echo "    align-content: center;";
         echo "    width: max(100%, calc({$this->pxlTileSize}px * $this->width));";
-        echo "    height: max(100%, calc({$this->pxlTileSize}px * $this->heigth));";
+        echo "    height: max(100%, calc({$this->pxlTileSize}px * $this->height));";
         echo "}";
         echo ".tile {";
         echo "    height: {$this->pxlTileSize}px;";
@@ -46,14 +47,51 @@ class Board
     {
         $this->inject_board_styles();
         echo '<div id="board-container">';
-        for ($i = 0; $i < $this->heigth; $i++) {
+        for ($i = 0; $i < $this->height; $i++) {
             for ($j = 0; $j < $this->width; $j++) echo '<div class="tile"></div>';
         }
         echo '</div>';
+    }
+
+    function placeSingleShip()
+    {
+        $valid = false;
+        $x = 0;
+        $y = 0;
+
+        while (!$valid) {
+            $x = rand(0, $this->width - 1);
+            $y = rand(0, $this->height - 1);
+
+            if ($this->tiles[$x][$y] === TileState::FREE) $valid = true;
+        }
+        $this->tiles[$x][$y] = TileState::OCCUPIED;
+        $this->excludeAdjacent($x, $y);
+        echo '<pre>';
+        print_r($this->tiles);
+        echo '</pre>';
+    }
+
+    function excludeAdjacent(int $x, int $y)
+    {
+        $this->excludeCell($x - 1, $y);
+        $this->excludeCell($x + 1, $y);
+        $this->excludeCell($x, $y - 1);
+        $this->excludeCell($x, $y + 1);
+    }
+
+    private function excludeCell(int $x, int $y)
+    {
+        if ($x >= 0 && $x < $this->width && $y >= 0 && $y < $this->height) {
+            if ($this->tiles[$x][$y] === TileState::FREE) {
+                $this->tiles[$x][$y] = TileState::EXCLUDED;
+            }
+        }
     }
 }
 
 $game = new Board();
 $game->create_board();
-
+$game->placeSingleShip();
+$game->placeSingleShip();
 ?>
